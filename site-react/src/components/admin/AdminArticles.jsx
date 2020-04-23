d29400c028ad62c95e93238436da80e82ce89d0f
luizinhovao <luiz.emof@gmail.com>
2020-04-23 11:29:14 -0300

    Create form to article admin page

--- site-react/src/components/admin/AdminArticles.jsx
+++ site-react/src/components/admin/AdminArticles.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function AdminArticles() {
-    return (
-        <div className="admin-article">
-            <h1>Admin articles</h1>
-        </div>
-    )
+export default class AdminArticles extends Component {
+    render() {
+        return (
+            <div>
+                {this.renderForm()}
+            </div>
+        )
+    }
+
+    renderForm() {
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="name">Nome:</label>
+                    <input
+                        id="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Informe o Nome do Artigo..." />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="description">Descrição:</label>
+                    <input
+                        id="description"
+                        type="text"
+                        className="form-control" 
+                        placeholder="Informe a Descrição do Artigo..." />
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="imageUrl">Image (URL)</label>
+                    <input 
+                        id="imageUrl" 
+                        className="form-control"
+                        type="text" 
+                        placeholder="Informe a URL da Imagem do Artigo..."/>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="categoryId">Categoria:</label>
+                    <select id="categoryId" className="form-control"></select>
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="userId">Usuário</label>
+                    <select className="form-control" id="userId"></select>
+                </div>
+            </form>
+        )
+    }
 }
